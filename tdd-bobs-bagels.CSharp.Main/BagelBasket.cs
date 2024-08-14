89e641f8bc27079bbfaa24904fdeefaeba832b0b
bjorgastrid <bjorgkrist@hotmail.com>
2024-08-14 14:59:15 +0200

    remove test passed

--- tdd-bobs-bagels.CSharp.Main/BagelBasket.cs
+++ tdd-bobs-bagels.CSharp.Main/BagelBasket.cs
@@ -17,8 +17,10 @@ namespace tdd_bobs_bagels.CSharp.Main
             return true;
         }
 
-        public bool remove(string v)
+        public bool remove(string bagel)
         {
+            bagles.Remove(bagel);
+
             return false;
         }
 
